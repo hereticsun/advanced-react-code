e5411b13a671f33046b87194a6afa599612f784c
Azlan Cuttilan <azlan@bynd.com>
2016-07-21 15:03:18 +0100

    Creating a JWT
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -12,7 +19,7 @@ exports.signup = function(req, res, next) {
     User.findOne({ email: email }, function(err, existingUser) {
         if (err) { return next(err);}
 
-        // If a user with email does exis, return an error
+        // If a user with email does exist, return an error
         if (existingUser) {
             return res.status(422).send({ error: 'Email is in use' });
         }
@@ -25,10 +32,10 @@ exports.signup = function(req, res, next) {
 
         user.save(function(err) {
             if(err) {return next(err); }
-        });
 
-        // Respond to request indicating the user was created
-        res.json({success: true});
+            // Respond to request indicating the user was created
+            res.json({ token: tokenForUser(user) });
+        });
     });
 
 
