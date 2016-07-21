2d3f310c7a2914bf3bdfa3951ceb815fdab2323c
Azlan Cuttilan <azlan@bynd.com>
2016-07-21 15:18:44 +0100

    Passport strategies
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

---
+++ server/services/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT Strategy
+const jwtOptions = {};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    User.findById(payload.sub, function(err, user) {
+        if(err) { return done(err, false); }
+
+        if(user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
+
+// Tell passport to use this strategy
